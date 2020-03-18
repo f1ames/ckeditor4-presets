b7cfdca6c8b7dc2c51a8ffd1a0496f9d9daa766d
Krzysztof Krztoń <k.krzton@cksource.com>
2020-03-18 16:33:39 +0100

    Updated update workflow.

--- dev/gh-actions/ckeditorupdate.sh
+++ dev/gh-actions/ckeditorupdate.sh
@@ -2,17 +2,13 @@
 # Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
 # For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 
-# Used actions
-# https://github.com/marketplace/actions/checkout
-# https://github.com/marketplace/actions/github-push
-
-# Update ckeditor submodule in active branch.
+# Updates "ckeditor" submodule in active branch - used by update.yml workflow.
 branch=$(git rev-parse --abbrev-ref HEAD)
 echo "Updating ckeditor4 submodule on ckeditor4-presets $branch branch..."
 
 git submodule update --init --recursive
 
-# Update ckeditor submodule branch.
+# Updates ckeditor submodule branch.
 echo "Fetching latest ckeditor4 changes from $branch branch..."
 
 cd "ckeditor"
@@ -21,15 +17,15 @@ git fetch --all -p
 git reset --hard "origin/$branch"
 newHash=$(git log -1 --format="%h")
 
-# Check if anything changed and push to remote.
+# Checks if anything changed and prepares commit.
 if [ "$oldHash" = "$newHash" ]; then
 	echo "Nothing changed (still on the latest $oldHash commit), aboritng..."
 else
-	echo "Updating $branch branch from $oldHash to $newHash..."
+	echo "Preparing update for $branch branch from $oldHash to $newHash..."
 
 	cd ".."
-	git config --local user.email "gh-actions@cksource.com"
-	git config --local user.name "CKSource GitHub Actions Bot"
+	git config --local user.email "ckeditor-bot@cksource.com"
+	git config --local user.name "CKEditor Bot"
 	git add ckeditor
 	git commit -m "Update CKEditor 4 submodule HEAD."
 fi
